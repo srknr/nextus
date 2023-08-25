d02da53b0d526e7e73ffea9ce65a5515d847002d
srknr <k_srkn13@yahoo.co.jp>
2023-08-25 09:04:49 +0900

    fix label

--- src/App.js
+++ src/App.js
@@ -1,23 +1,155 @@
-import logo from './logo.svg';
+import React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField'
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import NativeSelect from '@mui/material/NativeSelect';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <p></p>
+<div className="flexLayout">
+      <Box sx={{ minWidth: 80 }}>
+      <FormControl fullWidth>
+  <InputLabel variant="standard" htmlFor="uncontrolled-native">
+    店舗
+  </InputLabel>
+  <NativeSelect
+    defaultValue={0}
+    inputProps={{
+      name: 'shop',
+      id: 'uncontrolled-native',
+    }}
+  >
+    <option value={0}></option>
+    <option value={10}>SG</option>
+    <option value={20}>KS</option>
+    <option value={30}>UW</option>
+    <option value={40}>RU</option>
+    <option value={50}>YH</option>
+  </NativeSelect>
+</FormControl>
+</Box>
+        
+
+<Box sx={{ minWidth: 80 }}>
+      <FormControl fullWidth>
+  <InputLabel variant="standard" htmlFor="uncontrolled-native">
+    担当者
+  </InputLabel>
+  <NativeSelect
+    defaultValue={0}
+    inputProps={{
+      name: 'maneger',
+      id: 'uncontrolled-native',
+    }}
+  >
+    <option value={0}></option>
+    <option value={10}>F</option>
+    <option value={20}>HT</option>
+  </NativeSelect>
+</FormControl>
+</Box>
+
+<TextField  label="管理番号" variant="standard"/>
+</div>
+
+
+<p>
+      
+<Box sx={{ maxWidth: 180 }}>
+      <FormControl fullWidth>
+  <InputLabel variant="standard" htmlFor="uncontrolled-native">
+    商品状態
+  </InputLabel>
+  <NativeSelect
+    defaultValue={0}
+    inputProps={{
+      name: 'maneger',
+      id: 'uncontrolled-native',
+    }}
+  >
+    <option value={0}>新品未使用</option>
+    <option value={10}>未使用に近い</option>
+    <option value={20}>目立った傷汚れなし</option>
+    <option value={30}>傷汚れあり</option>
+    <option value={40}>全体的に状態が悪い</option>
+
+  </NativeSelect>
+</FormControl>
+</Box>
+</p>
+        
+ <div className="flexLayout">
+<Box sx={{ minWidth: 150 }}>
+      <FormControl fullWidth>
+  <InputLabel variant="standard" htmlFor="uncontrolled-native">
+    佐川orヤマトでの発送
+  </InputLabel>
+  <NativeSelect
+    defaultValue={0}
+    inputProps={{
+      name: 'maneger',
+      id: 'uncontrolled-native',
+    }}
+  >
+    <option value={0}></option>
+    <option value={10}>ネコポス</option> 
+    <option value={20}>コンパクト</option>
+    <option value={30}>60</option>
+    <option value={40}>80</option>
+    <option value={50}>100</option>
+    <option value={60}>140</option>
+    <option value={70}>160</option>
+    <option value={80}>170</option>
+    <option value={90}>180</option>
+    <option value={100}>200</option>
+    <option value={110}>220</option>
+    <option value={120}>240</option>
+    <option value={130}>260</option>
+
+  </NativeSelect>
+</FormControl>
+</Box>
+<Box sx={{ minWidth: 60 }}>
+      <FormControl fullWidth>
+  <InputLabel variant="standard" htmlFor="uncontrolled-native">
+    家財便
+  </InputLabel>
+  <NativeSelect
+    defaultValue={0}
+    inputProps={{
+      name: 'maneger',
+      id: 'uncontrolled-native',
+    }}
+  >
+    <option value={0}></option>
+    <option value={10}>S</option>
+    <option value={20}>A</option>
+    <option value={30}>B</option>
+    <option value={40}>C</option>
+    <option value={50}>D</option>
+    <option value={60}>E</option>
+    <option value={60}>F</option>
+
+  </NativeSelect>
+</FormControl>
+</Box>
+</div>
+      <TextField
+          id="standard-multiline-static"
+          label="備考欄"
+          multiline
+          rows={6}
+          sx={{ maxWidth: 300 }}
+          variant="standard"
+        />
       </header>
+
     </div>
   );
 }
